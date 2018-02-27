e3882872c459ceb2dc6c5890ead0783d0e3aa7c5
Josep Salvat <jsalvat87@gmail.com>
2018-02-27 13:39:42 +0100

    Almost ready frontend login/signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
+import { AuthService } from './services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+
+
+import { RequireAnonGuardService } from './guards/require-anon-guard.service';
+import { RequireUserGuardService } from './guards/require-user-guard.service';
+import { InitAuthGuardService } from './guards/init-auth-guard.service';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+// -- routes
+
+ const routes: Routes = [
+   { path: '',  component: AppComponent, canActivate: [ InitAuthGuardService ] },
+  //  { path: 'login',  component: AuthLoginPageComponent, canActivate: [ RequireAnonGuardService ] },
+  //  { path: 'signup',  component: AuthSignupPageComponent, canActivate: [ RequireAnonGuardService ] },
+  //  { path: 'page',  component: ... , canActivate: [ RequireUserGuardService ] },
+   { path: '**', redirectTo: '' }
+ ];
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [InitAuthGuardService,
+    RequireAnonGuardService,
+    RequireUserGuardService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
